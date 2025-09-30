8011a5f35a3d4d5ace63c492553419de13007f10
msv-akshat <saiakshat556@gmail.com>
2025-09-30 19:42:03 +0530

    test run

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import PlantDiseasePredictor from "./components/PlantDiseasePredictor";
 function App() {
   return (
     <div className="App d-flex align-items-center justify-content-center min-vh-100 bg-light">
-        <h1 className="text-center my-4">Plant Disease Detector 111</h1>
+        <h1 className="text-center my-4">Plant Disease Detector</h1>
         <PlantDiseasePredictor />
     </div>
   );
